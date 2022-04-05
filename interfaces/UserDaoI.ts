cbff6a19a893551ff1168af8e9335103fd51da48
Laura Culligan <lgculligan@gmail.com>
2022-04-05 19:05:57 -0400

    added search by username

--- interfaces/UserDaoI.ts
+++ interfaces/UserDaoI.ts
@@ -3,6 +3,7 @@ import User from "../models/users/User";
 export default interface UserDaoI {
     findAllUsers (): Promise<User[]>;
     findUserById (uid: string): Promise<any>;
+    searchUserByName (username: string): Promise<any>;
     createUser (user: User): Promise<User>;
     updateUser (uid: string, user: User): Promise<any>;
     deleteUser (uid: string): Promise<any>;
